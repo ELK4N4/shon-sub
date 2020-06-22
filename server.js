ff62d160abef369099dac4b1e2967ee08a76f92c
elkanagit <elkana.hendler@gmail.com>
2020-06-22 12:03:58 +0300

    fix dotenv crash

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 /* Import Modules */
-require('dotenv').config();
+if(process.env.NODE_ENV !== 'production') {}
+    require('dotenv').config();
+}
 
 const express = require('express');
 const favicon = require('serve-favicon');
