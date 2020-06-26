2203872f4dd999af714cdc398ae5fc4e4eea96e0
elkanagit <elkana.hendler@gmail.com>
2020-06-26 16:48:23 +0300

    trying again https script version

--- public/js/script.js
+++ public/js/script.js
@@ -9,11 +9,7 @@ function closeAllForms() {
   $(".fixed-form").removeClass("visible");
 }
 
-//force HTTPS 
-if (location.protocol != 'https:')
-{
- location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
-}
+
 
 window.onscroll = function() {
   scrollFunction()
@@ -47,6 +43,11 @@ $('.nav-btn').on('click', function() {
 
 
 $(document).ready(function(){
+  //force HTTPS 
+  if (location.protocol != 'https:')
+  {
+  location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+  }
 
   $(".closeForm").click(function(){
     closeAllForms();
