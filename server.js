dec91c0815aa4ba7c619bc8017a7d676d7052c7e
elkanagit <elkana.hendler@gmail.com>
2020-06-26 16:24:36 +0300

    trying again https

--- server.js
+++ server.js
@@ -46,7 +46,19 @@ db.on('error', error => console.log(error));
 db.once('open', () => {console.log('Mongoose is connected');});
 
 
-
+function checkHttps(req, res, next){
+    // protocol check, if http, redirect to https
+    
+    if(req.get('X-Forwarded-Proto').indexOf("https")!=-1){
+      console.log("https, yo")
+      return next()
+    } else {
+      console.log("just http")
+      res.redirect('https://' + req.hostname + req.url);
+    }
+  }
+  
+  app.all('*', checkHttps)
 /* Middleware */
 app.use('/', indexRouter);
 app.use('/projects', projectsRouter); //פרוייקטים
