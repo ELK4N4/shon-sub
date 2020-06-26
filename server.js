e623cba474a55521d7b708e3ad127d585678dcf8
elkanagit <elkana.hendler@gmail.com>
2020-06-26 16:38:38 +0300

    trying again https

--- server.js
+++ server.js
@@ -46,6 +46,20 @@ db.on('error', error => console.log(error));
 db.once('open', () => {console.log('Mongoose is connected');});
 
 
+// Make sure to put this directly after you define your app
+// dont change if you want http -> https
+// if you want https -> http change !req.secure to req.secure and https to http
+app.set('trust proxy', true); // <- required
+app.use((req, res, next) => {
+  if(!req.secure) return res.redirect('https://' + req.get('host') + req.url);
+  next();
+});
+
+// rest of this is just a demo
+app.use((req, res, next) => {
+  res.send(`HTTPS: ${req.secure}`); 
+  next();
+});
 
 /* Middleware */
 app.use('/', indexRouter);
